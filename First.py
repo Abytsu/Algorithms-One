00e2112fcd3c8c2106cf91cc15e25e01b6ecc9a4
Abytsu <42917989+Abytsu@users.noreply.github.com>
2021-11-26 12:22:23 -0300

    Redacted legacy

---
+++ First.py
@@ -0,0 +1,109 @@
+# (nome) 1 - Variáveis (caracteres)
+print("Olá, Mundo!")
+#Esse é meu nome 
+print("Lorem")
+# Esse é meu pai
+print("ipsum")
+# Essa é minha minha mãe
+print("dolor")
+# Essa é minha irmã
+print("sit")
+# Essa é meu irmão
+print("amet")
+# Esse é meu cachorro
+print("consectetur")
+# Essa é minha avó
+print("adipiscing")
+# Esse é meu avô
+print("elit")
+# Esse é meu primo
+print("sed")
+# Essa é minha prima
+print("do")
+# Essa é minha tia
+print("eiusmod")
+# Esse é meu tio
+print("tempor")
+# Esse é o incididunt
+x = 'incididunt'
+print(x)
+######################################
+# Parte 2 int (inteiros) (-2)
+print(-2+4)
+
+# Part 3 float (números reais) (2,5)
+print(20.50-5.5)
+
+# sempre se usa aspas para para caracteres, palavras, frases
+# python indentifica a varíavel através de: print(type(variável))
+print(type(3.21))
+print(type(-9))
+print(type("Michael Jackson"))
+
+# Parte 4 - Operações
+# Soma - print(x+y)
+print(10+5)
+
+# Subtração - print(x-y)
+print(10-5)
+
+# Divisão - print(x/y) ou (x//y)inteiro
+print(10/5)
+print(12//5)
+
+# Multiplicação - print(x*y)
+print(10*5)
+
+# Potência - print(x**y)
+print(10**5)
+
+# Resto - print(x%y)
+print(10%5)
+
+# Exercício 
+print(" ")
+print("Valor de:1.2*4/4+9-3.4")
+print(1.2*4/4+9-3.4)
+print('Marguerita') #Teste de string com aspas simples ''
+print(" ") # Espaço
+# Manipulação de string (str)
+print("""
+Roberto
+Carlos
+Bruna
+""")
+# \n para dar um pulo (espaço)
+print("ut labore \net dolore")
+#atribuir um valor à variável
+x=2
+y=3 
+z=x+y
+print(z)
+
+z1=x-y
+z2=x*y 
+print(z2)
+
+c=432
+a=-800
+print(a-y)
+
+print(c**y,z-z2,a//y)
+print(800//300,800/300)
+print(c%z2)
+###################
+# variável sendo uma string
+xz= "fernando de noronha"
+print(xz)
+print(xz[0])
+print(xz[1])
+print(xz[2])
+print(xz[3])
+print(xz[4])
+print(xz[5])
+print(xz[6])
+print(xz[7])
+print(xz[0]+xz[1]+xz[2]+xz[3]+xz[4]+xz[5]+xz[6]) # Concatenação (sempre começa com 0)
+print(xz[1:5])
+print(xz[1:3])
+print(xz[0:2])
